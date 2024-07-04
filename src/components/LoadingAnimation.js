d9fedcf197bec13fa53c1af6dea855629b70297a
Rohith Sampathi <skyguardian@Rohiths-MacBook-Pro.local>
2024-07-04 18:38:55 +0530

    Animation v1.15

--- src/components/LoadingAnimation.js
+++ src/components/LoadingAnimation.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LoadingAnimation = ({ elapsedTime }) => (
   <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
-    <img src="https://giphy.com/embed/YqopropuC719C" alt="Loading" className="w-64 h-64 mb-4" />
+    <img src="./1474.gif" alt="Loading" className="w-64 h-64 mb-4" />
     <p className="text-xl font-bold">Generating content...</p>
     <p className="text-lg">Time elapsed: {elapsedTime} seconds</p>
   </div>
